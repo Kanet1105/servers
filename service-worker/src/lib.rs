b2b2ce833b9a69067a745940c3bd794aeb4bea6e
Allen Clarence <zombiedelah@gmail.com>
2022-10-29 11:29:17 +0900

    Add missing main.rs.

--- service-worker/src/lib.rs
+++ service-worker/src/lib.rs
@@ -2,9 +2,10 @@ mod configs;
 mod data;
 
 use actix_web::{web, App, HttpServer, HttpRequest, HttpResponse, Responder};
-use actix_web_actors::ws;
+use actix_ws::Message;
 use configs::Configs;
 use data::AppData;
+use futures::stream::StreamExt;
 use tracing::info;
 
 pub async fn health_check(request: HttpRequest) -> impl Responder {
@@ -13,12 +14,30 @@ pub async fn health_check(request: HttpRequest) -> impl Responder {
 }
 
 pub async fn join_key(request: HttpRequest, app_data: web::Data<AppData>) -> impl Responder {
+    // info!("{:?}", app_data.join_key());
     HttpResponse::Ok()
 }
 
-pub async fn join(request: HttpRequest, stream: web::Payload) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn ws(request: HttpRequest, body: web::Payload) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+    let (response, mut session, mut message_stream) = actix_ws::handle(&request, body)?;
+    
+    actix_rt::spawn(async move {
+        while let Some(Ok(message)) = message_stream.next().await {
+            match message {
+                Message::Text(s) => {
+                    info!("{}", s);
+                },
+                _ => break,
+            }
+        }
+        let _ = session.close(None).await;
+    });
+    Ok(response)
 }
+
+// pub async fn join(request: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Box<dyn std::error::Error>> {
+//     let (response, session, message_stream) = actix_ws::handle(&request, stream)?;
+// }
 
 pub async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
     let configs = Configs::from_file("Configs.toml");
@@ -28,7 +47,7 @@ pub async fn run_app() -> Result<(), Box<dyn std::error::Error>> {
         App::new()
             .app_data(web::Data::new(AppData::new()))
             .route("/health_check", web::get().to(health_check))
-            .route("/join", web::post().to(join))
+            .route("/join", web::get().to(join_key))
     })
     .bind((configs.ip, configs.port))?
     .run()
