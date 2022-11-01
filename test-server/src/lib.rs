b3ac4f39ab874c603a7982c06f37b62b0aca1cf1
Allen Clarence <zombiedelah@gmail.com>
2022-11-01 18:22:56 +0900

    Echo server on top of tokio.

--- test-server/src/lib.rs
+++ test-server/src/lib.rs
@@ -1,18 +1,42 @@
 mod config;
-mod server;
 
+use bytes::Bytes;
 use config::Configuration;
-use std::sync::Arc;
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+use tokio::{
+    io::{self, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt},
+    net::TcpListener,
+};
+use tracing::info;
 
-pub type Exception = Box<dyn std::error::Error>;
+pub type Exception = Box<dyn std::error::Error + Send + Sync>;
+pub type Result<T> = std::result::Result<T, Exception>;
 
-pub async fn run() -> Result<(), Exception> {
+pub async fn run() -> Result<()> {
     let config = Arc::new(Configuration::from_file("./Config.toml"));
-    let listener = TcpListener::bind(&format!("{}:{}", config.ip, config.port)).await.unwrap();
+    let listener = TcpListener::bind(&format!("{}:{}", config.ip, config.port)).await?;
+    info!("The server's running on '{}:{}'", config.ip, config.port);
 
-    tokio::spawn(async {
+    loop {
+        let (stream, address) = listener.accept().await?;
+        let (mut reader, mut writer) = io::split(stream);
+        let mut buffer = vec![0; 128];
+        info!("Connected from {:?}", &address);
 
-    });
+        tokio::spawn(async move {
+            loop {
+                let n = reader.read(&mut buffer).await.unwrap();
+                if n == 0 {
+                    break;
+                }
+                info!("Got {:?}", &buffer[..n]);
+            }
+        });
+    }
+
     Ok(())
 }
--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -1,8 +1,8 @@
-use test_server::Exception;
+use test_server::Result;
 use tracing_subscriber::fmt::time;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-async fn main() -> Result<(), Exception> {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_timer(time::LocalTime::rfc_3339())
         .init();
