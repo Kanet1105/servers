14ca70d2088d0ebf333489e1fbce46fc58eacd35
Allen Clarence <zombiedelah@gmail.com>
2022-11-01 12:38:12 +0900

    Add local-time features to the logger.

--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -1,7 +1,11 @@
 use test_server::Exception;
+use tracing_subscriber::fmt::time;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() -> Result<(), Exception> {
+    tracing_subscriber::fmt()
+        .with_timer(time::LocalTime::rfc_3339())
+        .init();
     test_server::run().await?;
     Ok(())
 }
