e31dffccf3cd58b8489973eea5d262474b91a0c3
Allen Clarence <zombiedelah@gmail.com>
2022-11-05 17:46:38 +0900

    Add test serialization cases.

--- test-server/src/message.rs
+++ test-server/src/message.rs
@@ -1,52 +1,58 @@
 use bytes::{BytesMut, BufMut};
+use tokio::io::copy;
 
-pub enum MessageKind {
-    Text,
+pub trait Packet {
+    fn len(&self) -> usize;
+    fn serialize(&self) -> &[u8];
 }
 
 pub struct Message {
+    concurrency: usize,
+    requests: usize,
+    elapsed: f32,
     contents: String,
 }
 
-impl Message {
-    pub fn text(text: String) -> Self {
-        Self { contents: text }
-    }
-}
+// impl Packet for Message {
+//     fn len(&self) -> usize {
+//         (8 + 8 + 4) + self.contents.len() * 8
+//     }
 
-pub trait Packet {
-    fn size(&self) -> usize;
-    fn serialize(&self) -> BytesMut;
-}
+//     fn serialize(&self, buffer: &mut buffer) -> &[u8] {
+//         buffer.put_u8(self.concurrency);
+//         buffer.put_u8(self.requests);
+//         buffer.put_f32(self.elapsed);
+//     }
+// }
 
-impl Packet for Message {
-    fn size(&self) -> usize {
-        self.contents.len()
-    }
-
-    fn serialize(&self) -> BytesMut {
-        BytesMut::with_capacity(self.size())
-    }
-
-    // fn serialize(&self) -> &[u8] {
-    //     let mut buffer = BytesMut::with_capacity(self.size());
-    //     buffer.put(self.contents.as_bytes());
-    //     buffer
-    // }
-}
-
-pub unsafe fn as_u8_slice<T: Sized>(p: &T) -> &[u8] {
-    ::std::slice::from_raw_parts(
-        (p as *const T) as *const u8,
-        ::std::mem::size_of::<T>()
-    )
+#[test]
+fn serialization() {
+    let a = String::from("12345");
+    let mut buffer = BytesMut::with_capacity(20);
+    buffer.put_u64(1);
+    buffer.put_u64(13);
+    buffer.put_f32(1.252);
+    buffer.put_slice(a.as_bytes());
+    println!("{:?}", buffer);
+    println!("{:?}", buffer.len());
 }
 
 #[test]
-fn serializer() {
-    // let message = Message::text("Hello, world!".into());
-    // let buffer = unsafe { as_u8_slice(&message) };
-    let message = String::from("Hello, world!").as_bytes();
-    println!("{:?}", std::mem::size_of::<Message>());
-    // let original: String = unsafe { std::mem::transmute::<&[u8], String>(&*message) };
+fn serialize() {
+    let a: usize = 1;
+    let mut buffer_1 = BytesMut::with_capacity(1);
+    let mut buffer_2 = BytesMut::with_capacity(1);
+    buffer_1.put_slice(&a.to_be_bytes());
+    buffer_2.put_u64(1);
+    println!("{:?}", buffer_1);
+    println!("{:?}", buffer_2);
+    assert_eq!(buffer_1, buffer_2);
+}
+
+#[test]
+fn deserialize() {
+    let mut buffer = BytesMut::with_capacity(1);
+    buffer.put_slice(&(123 as u64).to_be_bytes());
+    buffer.put_slice(&(456 as u64).to_be_bytes());
+    
 }
