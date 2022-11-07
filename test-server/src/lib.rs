9372f6d1404bd93f4add3d0eaeb9686c26e2398f
Allen Clarence <zombiedelah@gmail.com>
2022-11-08 00:02:14 +0900

    Add message crate and fixed buffered reader.

--- test-server/src/lib.rs
+++ test-server/src/lib.rs
@@ -1,16 +1,14 @@
 mod config;
 mod data;
-mod message;
 
-use config::Configuration;
-use data::AppData;
+use bytes::{BytesMut, Buf, BufMut};
+use data::{AppData, SharedData};
+use message::{deserialize_with_capacity, SimpleText};
 use std::{
-    collections::HashMap,
     net::SocketAddr,
-    sync::{Arc, Mutex},
 };
 use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt},
+    io::{self, AsyncReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
     sync::mpsc,
 };
@@ -23,65 +21,41 @@ pub type Result<T> = std::result::Result<T, Exception>;
 pub async fn run() -> Result<()> {
     let data = AppData::new().with_config("./Config.toml").init();
     let ip_port = format!("{}:{}", data.config.ip, data.config.port);
-
-    // Bind
     let listener = TcpListener::bind(&ip_port).await?;
     info!("The server's running on '{}'", ip_port);
 
     loop {
         let (stream, address) = listener.accept().await?;
-        info!("Connected from {:?}", &address);
-        
-        let (mut reader, mut writer) = io::split(stream);
-        let mut buffer = vec![0; 16];
-
-        tokio::spawn(async move {
-            loop {
-                let n = match reader.read(&mut buffer).await {
-                    Ok(n) => n,
-                    Err(e) => {
-                        info!("{:?}", e);
-                        break;
-                    },
-                };
-                if n == 0 {
-                    break;
-                }
-                info!("Got {:?}", &buffer[..n]);
-            }
-        });
+        connection_handler(stream, address, data.clone());
     }
-
-    Ok(())
 }
 
-pub async fn connection_handler(stream: TcpStream) {
-    let (mut stream_reader, stream_writer) = io::split(stream);
-    let mut buffer = vec![0; 16];
+pub fn connection_handler(stream: TcpStream, address: SocketAddr, data: SharedData) {
+    let (mut read_stream, mut write_stream) = io::split(stream);
+    let mut buffer = BytesMut::with_capacity(8196);
+    let mut data = SimpleText {
+        channel: "".into(),
+        contents: "".into(),
+    };
 
+    // Broadcast to the channel belong.
     tokio::spawn(async move {
         loop {
-            let n = match stream_reader.read(&mut buffer).await {
+            let n = match read_stream.read_buf(&mut buffer).await {
                 Ok(n) => n,
                 Err(e) => {
                     info!("{:?}", e);
                     break;
-                },
+                }
             };
             if n == 0 {
                 break;
             }
-            info!("Message {:?}", &buffer[..n]);
+            info!("Read {} bytes", n);
+            deserialize_with_capacity(&mut buffer, &mut data).unwrap();
+            info!("{:?}", &data);
         }
     });
-}
 
-pub struct Connection {
-    id: String,
-    stream :TcpStream,
-    buffer: Vec<u8>,
-}
-
-impl Connection {
-
+    info!("Connected from {:?}", address);
 }
