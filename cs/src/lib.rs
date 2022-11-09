dbe7929844db02f3bf0de42f5ab776b3a23317cd
Allen Clarence <zombiedelah@gmail.com>
2022-11-09 13:37:56 +0900

    A basic udp client-server pattern.

--- cs/src/lib.rs
+++ cs/src/lib.rs
@@ -1,31 +1,36 @@
 mod config;
+
 pub mod prelude {
     pub type Error = Box<dyn std::error::Error>;
     pub type Result<T> = std::result::Result<T, Error>;
+
+    pub use crate::config::Configuration;
+
+    pub use bytes::{Buf, BufMut, BytesMut};
+    pub use std::{
+        io::{stdin, ErrorKind},
+        str::from_utf8,
+    };
+    pub use tokio::net::UdpSocket;
+    pub use tracing::{error, info};
 }
 
 use crate::prelude::*;
 
-use bytes::BytesMut;
-use config::Configuration;
-use std::{
-    io::ErrorKind,
-    str::from_utf8,
-};
-use tokio::net::UdpSocket;
-use tracing::{error, info};
-
-pub async fn run_udp() -> Result<()> {
+pub async fn run_server() -> Result<()> {
     let config = Configuration::from_file("./Config.toml");
-    let sock = UdpSocket::bind(format!("{}:{}", config.ip, config.port)).await?;
-    info!("Starting the server on {:?}", sock.local_addr()?);
+    let socket = UdpSocket::bind(format!("{}:{}", config.ip, config.port)).await?;
+    info!("Starting the server on {:?}", socket.local_addr()?);
     let mut buffer = BytesMut::with_capacity(8192);
 
     loop {
-        match sock.try_recv_buf(&mut buffer) {
+        match socket.try_recv_buf(&mut buffer) {
             Ok(n) => {
-                let message = from_utf8(&buffer)?;
-                info!("Received {} bytes: {}", n, message);
+                let len = buffer.get_u64() as usize;
+                info!("Message length : {}", len);
+                let text = from_utf8(&buffer[0..len])?;
+                info!("Received {} bytes: {:?}", n, text);
+                buffer.advance(len);
             },
             Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
                 continue;
@@ -35,5 +40,21 @@ pub async fn run_udp() -> Result<()> {
                 return Err(Box::new(e));
             },
         }
+    }
+}
+
+pub async fn run_client() -> Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:50001").await?;
+    socket.connect("127.0.0.1:50000").await?;
+    let mut buffer = BytesMut::with_capacity(8);
+
+    loop {
+        let mut text = String::new();
+        stdin().read_line(&mut text).unwrap();
+        
+        buffer.put_u64(text.trim().len() as u64);
+        buffer.put_slice(text.trim().as_bytes());
+        socket.try_send(buffer.as_ref())?;
+        buffer.clear();
     }
 }
